89d0df11b29193d0a6dd447b9b072cde8e04bdea
COWGRAMMAR <rafifryanputra189@gmail.com>
2026-03-09 22:03:03 +0700

    first comit

---
+++ resource/logic.cpp
@@ -0,0 +1,8 @@
+#include "dungeon.h"
+#include <iostream>
+#include <raylib.h>
+
+void sayhello()
+{
+    std::cout << "sayhello";
+}
---
+++ resource/main.cpp
@@ -0,0 +1,10 @@
+#include "dungeon.h"
+#include <iostream>
+#include <raylib.h>
+
+int main(int argc, char const *argv[])
+{
+    sayhello();
+    system("pause");
+    return 0;
+}
