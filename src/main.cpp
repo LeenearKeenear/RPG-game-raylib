0cb8e4d15f9a0ce953cb57b7647ce48276cfe24a
NiskoDT <65821447+NiskoDT@users.noreply.github.com>
2026-04-06 14:24:51 +0700

    Integrate main menu into game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/dungeon.h"
 #include "../include/screen.h"
 #include "../include/map.h"
+#include "../include/mainMenu.h"
 #include "../lib/raylib/include/raylib.h"
 #include "../lib/raylib/include/raymath.h"
 
@@ -8,13 +9,22 @@ int main()
 {
     GameState state = InitScreen();
     InitDrawMap(&state);
+    InitMainMenu(&state);
 
     while (!WindowShouldClose())
     {
-        UpdateGame(&state);
-        UpdatePlayer(&state);
-        DrawRenderTexture(&state);
-        DrawRenderWindows(&state);
+        if (state.currentScreen == MAIN_MENU)
+        {
+            UpdateMainMenu(&state);
+            DrawMainMenu(&state);
+        }
+        else if (state.currentScreen == PLAY)
+        {
+            UpdateGame(&state);
+            UpdatePlayer(&state);
+            DrawRenderTexture(&state);
+            DrawRenderWindows(&state);
+        }
     }
 
     GameShutDown(&state);
